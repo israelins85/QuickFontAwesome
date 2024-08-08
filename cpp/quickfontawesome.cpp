1cbcd6d66bdef05790ecb52d78d6a538e87de1c9
Israel Lins Albuquerque <i.lins85@icloud.com>
2024-08-08 17:32:31 +0200

    invalid include

--- cpp/quickfontawesome.cpp
+++ cpp/quickfontawesome.cpp
@@ -1,9 +1,8 @@
-#include "quickflux.hpp"
-
 #include <QQmlApplicationEngine>
 #include <QUrl>
 
 void qmlRegisterQuickFontAwesomeTypes() {
-    qmlRegisterSingletonType(QUrl("qrc:///QuickFontAwesome/QuickFontAwesome.qml"), "QuickFontAwesome", 1, 0,
-                             "QuickFontAwesome");
+    qmlRegisterSingletonType(
+        QUrl("qrc:///QuickFontAwesome/QuickFontAwesome.qml"), "QuickFontAwesome", 1, 0,
+        "QuickFontAwesome");
 }
